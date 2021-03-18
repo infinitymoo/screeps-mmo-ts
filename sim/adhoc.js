a10687c844b1e7a53fa67fac1302caca6e561963
infinitymoo <infinitymoo@yahoo.co.uk>
2021-03-18 02:47:23 +0200

    Basic transpiling working and mocha is running

--- sim/adhoc.js
+++ sim/adhoc.js
@@ -1,11 +1,15 @@
-/** 
+"use strict";
+exports.__esModule = true;
+exports.helloWorld = void 0;
+/**
 * Tests basic TS setup and variable passing.
 * @param {string} var1 - variable being passed for testing.
 * @return {string} first variable modified to complete test
 */
-export const helloWorld = function(var1:string):string {
-    let response = `hello world: ${var1} received`;
+var helloWorld = function (var1) {
+    var response = "hello world: " + var1 + " received";
     // if( Game !== null )
     //     response += ` and can list game rooms ${JSON.stringify(Game.rooms)}`;
     return response;
-}
+};
+exports.helloWorld = helloWorld;
--- sim/main.js
+++ sim/main.js
@@ -1,14 +1,10 @@
-import * as adhoc from "./adhoc";
-
-
-
+"use strict";
+exports.__esModule = true;
+var adhoc = require("./adhoc");
 // module.exports.loop = function () {
 //     masterForeman.helloWorld();
-    
 // }
-
 function mainLoop() {
     console.log(adhoc.helloWorld("test message"));
 }
-
 mainLoop();
